21e31c795f488dcb53af4578fa49a2355780aa9a
“faizanalibaig” <riazfiaz6181@gmail.com>
2024-05-30 00:05:23 +0500

    drop down menu

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,19 +1,53 @@
-import React from 'react'
-import Image from 'next/image'
-import MenubarImage from '@/public/navbar/menubar.png'
+'use client'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import MenubarImage from '@/public/navbar/menubar.png';
 import { RiMenuLine } from "react-icons/ri";
 
+const menuoption=[
+  {
+    id:1,
+    title:'home'
+  },{
+    id:2,
+    title:'contact'
+  },{
+    id:3,
+    title:'about'
+  },{
+    id:4,
+    title:'resume'
+  }
+]
 
-const Navbar = () => {
+const Navbar= () => {
+  const [menu, setMenu] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setMenu(prevMenu => !prevMenu);
+  }
+
   return (
-    <nav className='px-4 py-4 bg-[#1A1C1B] text-[#FFFFFF]'>
-      <header className='flex justify-between items-center'>
-        <h1 className='text-[18px] font-medium'>faizanali./{'>'}</h1>
-        {/* <Image src={MenubarImage} alt="menu icon" className='w-auto h-'/> */}
-        <RiMenuLine className=' rounded-none text-[18px]'/>
+    <nav className='bg-[#1A1C1B] text-[#FFFFFF] relative'>
+      <header className='px-5 py-5 flex justify-between items-center'>
+        <h1 className='text-[22px] font-medium'>faizanali./{'>'}</h1>
+        <RiMenuLine onClick={toggleMenu} className='rounded-none text-[22px]' />
       </header>
-   </nav>
-  )
+      {
+       menu && 
+       <div className='bg-[#1A1C1B] h-[200px] w-full absolute z-20 top-0 px-5 py-5 flex flex-col justify-between'>
+         <h1 className='text-[22px] font-medium'>faizanali./{'>'}</h1>
+         <section className='flex justify-between'>
+          {menuoption.map((value)=>{
+            return(
+              <p key={value.id} className='border-b text-[18px]'>{`${value.title} />`}</p>
+            )
+          })}
+         </section>
+       </div>
+      }
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
