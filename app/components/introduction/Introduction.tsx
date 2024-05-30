1513ef9d36f68b3a35e4ffd7db7635c11b8ede28
“faizanalibaig” <riazfiaz6181@gmail.com>
2024-05-30 17:49:29 +0500

    navbar working

--- app/components/introduction/Introduction.tsx
+++ app/components/introduction/Introduction.tsx
@@ -12,7 +12,7 @@ const Introduction = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      const height = window.innerHeight * 0.92; 
+      const height = window.innerHeight * 0.91; 
       setSectionHeight(`${height}px`);
     };
 
@@ -38,11 +38,11 @@ const Introduction = () => {
        <section className='w-full h-[60%] flex flex-col justify-between'>
          <div className='px-5'>
          <h1 className='text-[38px] font-semibold leading-10 mb-5'>mernstack <br/> developer</h1>
-         <p className='text-[15px] pb-3'>
+         <p className='text-[16px] pb-3'>
          I am a passionate computer science student with a strong focus on web development particularly in front-end technologies. Coding is not just a skill but a love I actively pursue daily. Since Feb 2023, I have been dedicated to web development, thriving in both solo and team projects.</p>
          </div>
 
-         <div className='text-[15px]'>
+         <div className='text-[18px]'>
             <button className='bg-[#DC5F00] w-[50%] py-4'>contact/{'>'}</button>
             <button className='bg-[#777676] w-[50%] py-4'>github/{'>'}</button>
          </div>   
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import MenubarImage from '@/public/navbar/menubar.png';
 import { RiMenuLine } from "react-icons/ri";
+import { LuPanelBottomClose } from "react-icons/lu";
 
 const menuoption=[
   {
@@ -23,20 +24,27 @@ const menuoption=[
 const Navbar= () => {
   const [menu, setMenu] = useState<boolean>(false);
 
-  const toggleMenu = () => {
-    setMenu(prevMenu => !prevMenu);
+  const openMenu = () => {
+    setMenu(true);
+  }
+
+  const closeMenu = () => {
+    setMenu(false);
   }
 
   return (
     <nav className='bg-[#1A1C1B] text-[#FFFFFF] relative'>
       <header className='px-5 py-5 flex justify-between items-center'>
-        <h1 className='text-[22px] font-medium'>faizanali./{'>'}</h1>
-        <RiMenuLine onClick={toggleMenu} className='rounded-none text-[22px]' />
+        <h1 className='text-[20px] font-medium'>faizanali./{'>'}</h1>
+        <RiMenuLine onClick={openMenu} className='rounded-none text-[20px]' />
       </header>
       {
        menu && 
-       <div className='bg-[#1A1C1B] h-[200px] w-full absolute z-20 top-0 px-5 py-5 flex flex-col justify-between'>
-         <h1 className='text-[22px] font-medium'>faizanali./{'>'}</h1>
+       <div className='bg-[#1A1C1B] border-b border-[#777676] h-[200px] w-full absolute z-20 top-0 px-5 py-8 flex flex-col justify-between'>
+         <div className='flex items-center justify-between'>
+          <h1 className='text-[22px] font-medium'>faizanali./{'>'}</h1>
+          <LuPanelBottomClose className='text-[22px]' onClick={closeMenu}/>
+         </div> 
          <section className='flex justify-between'>
           {menuoption.map((value)=>{
             return(
